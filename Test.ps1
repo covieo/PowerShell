758a05bf704a7da54766d91fb420edb72293bc47
Jeremy Covieo <jeremy.mic.covieo@uspsector.com>
2018-04-16 15:58:22 -0400

    Testing new PC

--- Test.ps1
+++ Test.ps1
@@ -1,2 +1,3 @@
 cd 'C:\Users\YOGA\Documents\GitHub\PowerShell'
 .\'01 - Delete DNS Records.ps1' -domainFQDN 'test.com' -ServerInput ServerName -vmDnsServersInput '10.10.1.1','10.10.1.2','10.10.1.3'  
+#.\'01 - Delete DNS Records.ps1' -domainFQDN 'test.com' -ServerInput ServerName -vmDnsServersInput '10.10.1.1','10.10.1.2','10.10.1.3'  
