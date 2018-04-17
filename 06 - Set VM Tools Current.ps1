d231250de3149f06cc36712e0f7adcb1bfe86f4f
Jeremy Covieo <jeremy.mic.covieo@uspsector.com>
2018-04-16 20:36:21 -0400

    Added Enable Trust for Kerberos Delegation in AD

--- 06 - Set VM Tools Current.ps1
+++ 06 - Set VM Tools Current.ps1
@@ -5,27 +5,27 @@ param ( [Parameter(mandatory=$true)]$ServerInput,
 . .\'Result Log.ps1'
 $Server = Connect-VIServer -Server $vmhost -User $user -Password $password
 # Network interface card (NIC) adapter.
-write_to_log -ServerInput $ServerInput "    - Network interface card (NIC) adapter type is VMXNET3."
+write_to_log -ServerInput $ServerInput -log_msg "    - Network interface card (NIC) adapter type is VMXNET3."
 $oldAdapter = Get-NetworkAdapter -VM $ServerInput
 Try {
     Set-NetworkAdapter  -NetworkAdapter $oldAdapter -WakeOnLan:$false -StartConnected:$true -Type 'Vmxnet3' -Connected:$true -RunAsync:$false -Confirm:$false  
-    write_to_log -ServerInput $ServerInput "    Success - Network interface card (NIC) adapter set successfully."
+    write_to_log -ServerInput $ServerInput -log_msg "    Success - Network interface card (NIC) adapter set successfully."
 } 
 catch [System.Exception] {
-    write_to_log -ServerInput $ServerInput $_.Exception.Message -is_error $true
+    write_to_log -ServerInput $ServerInput -log_msg $_.Exception.Message -is_error $true
 }
 # Step 06 - Set VM Tools Current
-write_to_log -ServerInput $ServerInput "Step 06 - Set VM Tools Current"
+write_to_log -ServerInput $ServerInput -log_msg "Step 06 - Set VM Tools Current"
 Try {
     $ToolsInstalled = (Get-VM -Name "$ServerInput" | Get-View).Guest.ToolsVersionStatus
     if ($ToolsInstalled -ne "guestToolsCurrent") {
         Get-VM $ServerInput | Update-Tools –NoReboot
     }
     if ($ToolsInstalled -eq "guestToolsCurrent") {
-        write_to_log -ServerInput $ServerInput "    Success - Step  06 - Set VM Tools Current."
+        write_to_log -ServerInput $ServerInput -log_msg "    Success - Step  06 - Set VM Tools Current."
     }
 } 
 catch [System.Exception] {
-    write_to_log -ServerInput $ServerInput $_.Exception.Message -is_error $true
+    write_to_log -ServerInput $ServerInput -log_msg $_.Exception.Message -is_error $true
 }
 Disconnect-VIServer -Server $Server -confirm:$false
--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,4 @@
 cd 'C:\Users\YOGA\Documents\GitHub\PowerShell'
 .\'01 - Delete DNS Records.ps1' -domainFQDN 'test.com' -ServerInput ServerName -vmDnsServersInput '10.10.1.1','10.10.1.2','10.10.1.3'  
 #.\'01 - Delete DNS Records.ps1' -domainFQDN 'test.com' -ServerInput ServerName -vmDnsServersInput '10.10.1.1','10.10.1.2','10.10.1.3'  
+#"CN=Computers,DC=test,DC=com"
